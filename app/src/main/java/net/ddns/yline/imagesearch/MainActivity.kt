b7f4f38bcbd93829a546eec7cfd51d68bd0f42a5
yline <ryuyoseon@rysMAC.local>
2021-09-17 21:43:19 +0900

    add permission, modify main
    - add internet permission
    - create searchView & textview
    - url local.properties로 이동

--- app/src/main/java/net/ddns/yline/imagesearch/MainActivity.kt
+++ app/src/main/java/net/ddns/yline/imagesearch/MainActivity.kt
@@ -2,6 +2,13 @@ package net.ddns.yline.imagesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.widget.SearchView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import net.ddns.yline.imagesearch.databinding.ActivityMainBinding
 
 // class name = .link--h3bPW
@@ -12,8 +19,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.searchImg.isSubmitButtonEnabled = true
+        binding.searchImg.setOnQueryTextListener(QueryTextListener())
 
+        getItemList()
+    }
+
+    private fun getItemList(){
+        CoroutineScope(Dispatchers.Main).launch {
+            Log.d("coroutine main start", "${this}")
+
+            getImageInformation()
+
+            Log.d("coroutine main end", "end")
+        }
+    }
+
+    inner class QueryTextListener:SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            Log.d("||search view submit", "${query}")
+            return true
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            Log.d("||search view text change", "${newText}")
+            return true
+        }
     }
 }
 
-//suspend fun getImageInformation():
+suspend fun getImageInformation():Boolean = withContext(Dispatchers.IO){
+    Log.d("coroutine io start", this.toString())
+
+
+
+    Log.d("coroutine io end", "end")
+    true
+}
