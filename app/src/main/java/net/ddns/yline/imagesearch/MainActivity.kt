518f5cc8fd5f558e30aba93aa51a853c8f8e2123
yline <ryuyoseon@rysMAC.local>
2021-09-17 15:20:51 +0900

    default setting

--- app/src/main/java/net/ddns/yline/imagesearch/MainActivity.kt
+++ app/src/main/java/net/ddns/yline/imagesearch/MainActivity.kt
@@ -2,10 +2,18 @@ package net.ddns.yline.imagesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.ddns.yline.imagesearch.databinding.ActivityMainBinding
+
+// class name = .link--h3bPW
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
     }
 }
+
+//suspend fun getImageInformation():
